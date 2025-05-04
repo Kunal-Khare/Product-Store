678a222c16ecf53ca89da3b1f0bbc14fe338922f
Kunal-Khare <kunalkhare47@gmail.com>
2025-05-04 23:54:19 +0530

    small-changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,24 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-    const app = express();
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // for security --> helmet is a security middleware that helps to protect your app by setting various HTTP headers
+app.use(morgan("dev"))
+app.get('/test', (req, res) => {
+    console.log(res.getHeaders());
+    res.send('hello from the test')
+});
+
+app.listen(PORT, () => {
+    console.log('server is running' + PORT);
+}); 
